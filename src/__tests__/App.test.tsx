8ddf526d26837bb3b8c035bdf29a98cfd487731f
veronikar <veronika.rosicova@Mac.zonalfree.zonal>
2025-06-27 14:52:26 +0100

    refactor: move app test into folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 test("renders hello message", () => {
